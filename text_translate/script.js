b1acf99fd555adfe229df169f351e8ecbb29b77e
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2025-01-09 08:02:19 +0530

    image update

--- text_translate/script.js
+++ text_translate/script.js
@@ -1,63 +1,63 @@
 const selectTag = document.querySelectorAll("select");
 const translateBtn = document.querySelector("#transfer");
+const resetBtn = document.querySelector("#reset");
 const fromText = document.querySelector("#fromText");
 const toText = document.querySelector("#toText");
 const icons = document.querySelectorAll("img");
 
-
-
-selectTag.forEach((tag , id)=>{
-    
-
-    for (const countriesCode in countries) {
-        let selected ;
-        if( id == 0 && countriesCode == "en-GB"){
-            selected =" selected";
-        }else if(id == 1 && countriesCode == "hi-IN"){
-            selected =" selected";
-        }
-        let option = ` <option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-        }
-});
-
-translateBtn.addEventListener("click" , ()=> {
-  let Text = fromText.value,
-  translateFrom = selectTag[0].value,
-  translateTo = selectTag[1].value;
-
-  let apiURL= `https://api.mymemory.translated.net/get?q=${Text}!&langpair=${ translateFrom}|${translateTo}`;
-  
-  fetch(apiURL).then(res => res.json()).then(data =>{
-
- 
-    toText.value = data.responseData.translatedText;
-});
- 
-
-});
-
-icons.forEach(icon =>{
-icon.addEventListener( "click" , ({target}) => {
-    if(target.classList.contains("copy") ){
-        if(target.id == "from"){
-           navigator.clipboard.writeText(fromText.value)
-        }
-        else{
-            navigator.clipboard.writeText(toText.value)
-        }
+selectTag.forEach((tag, id) => {
+  for (const countryCode in countries) {
+    let selected;
+    if (id === 0 && countryCode === "en-GB") {
+      selected = "selected";
+    } else if (id === 1 && countryCode === "hi-IN") {
+      selected = "selected";
     }
-    else{
-        let utterance;
-        if(target.id == "from"){
-           utterance = new SpeechSynthesisUtterance(fromText.value)
-           utterance.lang = selectTag[0].value;
-
-        }else{
-            utterance = new SpeechSynthesisUtterance(toText.value)
-            utterance.lang = selectTag[1].value;
-        }
-        speechSynthesis.speak(utterance);
-    }
+    let option = `<option value="${countryCode}" ${selected}>${countries[countryCode]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
 });
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+
+  let apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((data) => {
+      toText.value = data.responseData.translatedText;
+    });
+});
+
+// Reset functionality
+resetBtn.addEventListener("click", () => {
+  fromText.value = "";
+  toText.value = "";
+  selectTag[0].value = "en-GB";
+  selectTag[1].value = "hi-IN";
+});
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("copy")) {
+      if (target.id === "from") {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(toText.value);
+      }
+    } else {
+      let utterance;
+      if (target.id === "from") {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTag[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(toText.value);
+        utterance.lang = selectTag[1].value;
+      }
+      speechSynthesis.speak(utterance);
+    }
+  });
 });
